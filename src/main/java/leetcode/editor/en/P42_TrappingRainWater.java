798ad483384e850a33747d044c6c1997210b3cc8
cs1dada <cse3dadakiller@gmail.com>
2022-03-15 23:32:34 +0800

    P42

---
+++ src/main/java/leetcode/editor/en/P42_TrappingRainWater.java
@@ -0,0 +1,22 @@
+package leetcode.editor.en;
+
+// P42_TrappingRainWater
+
+  public class P42_TrappingRainWater {
+      
+      public static void main(String[] args) {
+      
+           Solution solution = new P42_TrappingRainWater().new Solution();
+           
+      }
+      
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int trap(int[] height) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+      
+  }
