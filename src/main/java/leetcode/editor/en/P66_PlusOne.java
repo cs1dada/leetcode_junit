a8535cc6af9774aa6fd1922e89b51ad95e87df6b
cs1dada <cse3dadakiller@gmail.com>
2022-03-17 17:09:48 +0800

    p66

---
+++ src/main/java/leetcode/editor/en/P66_PlusOne.java
@@ -0,0 +1,22 @@
+package leetcode.editor.en;
+
+// P66_PlusOne
+
+  public class P66_PlusOne {
+      
+      public static void main(String[] args) {
+      
+           Solution solution = new P66_PlusOne().new Solution();
+           
+      }
+      
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] plusOne(int[] digits) {
+        return digits;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+      
+  }
