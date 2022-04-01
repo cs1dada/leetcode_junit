8c428e42390c3e016994eaf27a2f43699a464af5
cs1dada <cse3dadakiller@gmail.com>
2022-04-01 17:26:50 +0800

    3

--- src/main/java/leetcode/editor/en/P3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/leetcode/editor/en/P3_LongestSubstringWithoutRepeatingCharacters.java
@@ -13,7 +13,32 @@ public class P3_LongestSubstringWithoutRepeatingCharacters {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int lengthOfLongestSubstring(String s) {
-            return 0;
+            int max = 0;
+            if (s.length() <= 1)
+                return s.length();
+
+            for (int i = 0; i < s.length() - 1; i++) {
+                for (int j = i + 1; j < s.length() - 1; j++) {
+                    int tmp = subTest(i, j, s);
+                    System.out.println(tmp);
+                    if (tmp > max) {
+                        max = tmp;
+                    }
+                }
+            }
+            return max;
+        }
+
+        int subTest(int i, int j, String s) {
+            int[] charHashTable = new int[128];
+            for (int k = i; k < j + 1; k++) {
+                char tmpChar = s.charAt(k);
+                charHashTable[tmpChar]++;
+                if (charHashTable[tmpChar] >= 2) {
+                    return k - i + 1;
+                }
+            }
+            return j - i + 1;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/leetcode/editor/en/P3_LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,28 @@
+package leetcode.editor.en;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P3_LongestSubstringWithoutRepeatingCharactersTest {
+
+    P3_LongestSubstringWithoutRepeatingCharacters.Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new P3_LongestSubstringWithoutRepeatingCharacters().new Solution();
+    }
+
+    @Test
+    void testTrap() {
+        String s = "abcabcbb";
+        assertEquals(3,
+                solution.lengthOfLongestSubstring("abcabcbb"));
+        assertEquals(1,
+                solution.lengthOfLongestSubstring("bbbbb"));
+        assertEquals(3,
+                solution.lengthOfLongestSubstring("pwwkew"));
+
+    }
+}
