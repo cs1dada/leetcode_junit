068ec84c7d7d916f501d2eab803fbc77159c4446
cs1dada <cse3dadakiller@gmail.com>
2022-04-07 21:54:07 +0800

    125 first

---
+++ src/main/java/leetcode/editor/en/P125_ValidPalindrome.java
@@ -0,0 +1,23 @@
+package leetcode.editor.en;
+
+// P125_ValidPalindrome
+
+public class P125_ValidPalindrome {
+
+    public static void main(String[] args) {
+
+        Solution solution = new P125_ValidPalindrome().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPalindrome(String s) {
+            return true;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
