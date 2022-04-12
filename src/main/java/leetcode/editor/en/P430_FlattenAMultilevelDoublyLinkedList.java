03414290d67374ddad979bfe72643754f9cdbdaf
cs1dada <cse3dadakiller@gmail.com>
2022-04-12 21:48:23 +0800

    430 first

---
+++ src/main/java/leetcode/editor/en/P430_FlattenAMultilevelDoublyLinkedList.java
@@ -0,0 +1,35 @@
+package leetcode.editor.en;
+
+// P430_FlattenAMultilevelDoublyLinkedList
+
+import leetcode.editor.en.LinkList.Node;
+
+public class P430_FlattenAMultilevelDoublyLinkedList {
+      
+      public static void main(String[] args) {
+      
+           Solution solution = new P430_FlattenAMultilevelDoublyLinkedList().new Solution();
+           
+      }
+      
+      //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        return head;
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+      
+  }
