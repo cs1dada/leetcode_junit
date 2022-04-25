50224b40a33b012a1dc031b92c92573930d9960b
cs1dada <cse3dadakiller@gmail.com>
2022-04-25 15:53:40 +0800

    20 first

---
+++ src/main/java/leetcode/editor/en/P20_ValidParentheses.java
@@ -0,0 +1,22 @@
+package leetcode.editor.en;
+
+// P20_ValidParentheses
+
+public class P20_ValidParentheses {
+
+    public static void main(String[] args) {
+
+        Solution solution = new P20_ValidParentheses().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isValid(String s) {
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
---
+++ src/test/java/leetcode/editor/en/P20_ValidParenthesesTest.java
@@ -0,0 +1,14 @@
+package leetcode.editor.en;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P20_ValidParenthesesTest {
+    P20_ValidParentheses.Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new P20_ValidParentheses().new Solution();
+    }
+}
