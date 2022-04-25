00cf047214e17f8d9d245d6dce3aaff685035c76
cs1dada <cse3dadakiller@gmail.com>
2022-04-25 15:48:04 +0800

    142 math solution

--- src/main/java/leetcode/editor/en/P142_LinkedListCycleII.java
+++ src/main/java/leetcode/editor/en/P142_LinkedListCycleII.java
@@ -27,7 +27,32 @@ public class P142_LinkedListCycleII {
      */
     public class Solution {
         public ListNode detectCycle(ListNode head) {
-            return head;
+
+            ListNode oneStep = head;
+            ListNode twoStep = head;
+            while (twoStep != null && twoStep.next != null) {
+                oneStep = oneStep.next;
+                twoStep = twoStep.next.next;
+
+                //cycle exist
+                if (oneStep == twoStep) {
+                    //數學公式
+                    // head a {q} b {p}
+                    //         |     |
+                    //         -  c  -
+                    // 2(a+b) = a+2b+c
+                    // a = c
+                    // slow從p往q走的距離(c) 跟 slow2從head往q走的距離(a) 一樣(速度也一樣) ~~~
+                    ListNode oneStep2 = head;
+                    while (oneStep != oneStep2) {
+                        System.out.println(oneStep2.val + " " + oneStep.val);
+                        oneStep = oneStep.next;
+                        oneStep2 = oneStep2.next;
+                    }
+                    return oneStep;
+                }
+            }
+            return null;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
--- src/test/java/leetcode/editor/en/P142_LinkedListCycleIITest.java
+++ src/test/java/leetcode/editor/en/P142_LinkedListCycleIITest.java
@@ -1,5 +1,6 @@
 package leetcode.editor.en;
 
+import leetcode.editor.en.LinkList.ListNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class P142_LinkedListCycleIITest {
 
     P142_LinkedListCycleII.Solution solution;
+    ListNode node;
 
     @BeforeEach
     void setUp() {
@@ -16,6 +18,32 @@ class P142_LinkedListCycleIITest {
 
     @Test
     void test() {
+        node = case2();
+        System.out.println(solution.detectCycle(node) != null ? solution.detectCycle(node).val : "null");
+
+    }
+
+    ListNode case1() {
+        // 3 -> 2 -> 0 -> -4
+        //      ^          |
+        //      |----------|
+        //2   0
+        //0   2
+        //-4  -4
+
+        ListNode a = new ListNode(3);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(0);
+        ListNode d = new ListNode(-4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = b;
+        return a;
+    }
+
+    ListNode case2() {
+        return null;
 
     }
 }
