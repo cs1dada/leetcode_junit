919db1c83844320381d23d8008ee47d93115d622
cs1dada <cse3dadakiller@gmail.com>
2022-05-10 16:39:18 +0800

    215

--- src/main/java/leetcode/editor/en/P215_KthLargestElementInAnArray.java
+++ src/main/java/leetcode/editor/en/P215_KthLargestElementInAnArray.java
@@ -16,8 +16,23 @@ public class P215_KthLargestElementInAnArray {
         //selection sort
 
         public int findKthLargest(int[] nums, int k) {
+            int length = nums.length - 1;
+            quickSort(nums, 0, length);
+            return nums[length - k];
+        }
 
-            return 0;
+        void quickSort(int[] nums, int start, int end) {
+            int length = nums.length;
+            int pivot = partion(nums, 0, length - 1);
+            quickSort(nums, start, pivot - 1);
+            quickSort(nums, pivot + 1, end);
+
+        }
+
+        int partion(int[] nums, int start, int end) {
+            int pivot = 0;
+
+            return pivot;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
